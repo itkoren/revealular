46b901ff7b684302e5e4c59fd851ad90b608d6a5
itkoren <itkoren@gmail.com>
2014-04-23 11:36:19 +0300

    Fix load event handler for browser console speaker notes plugin

--- init2.js
+++ init2.js
@@ -1,21 +1,23 @@
 // Full list of configuration options available here:
 // https://github.com/hakimel/reveal.js#configuration
+var queryHash = Reveal.getQueryHash();
 Reveal.initialize({
     controls: true,
     progress: true,
     history: true,
     center: true,
-    theme: Reveal.getQueryHash().theme,
-    transition: Reveal.getQueryHash().transition || 'default',
+    theme: queryHash.theme || 'sky',
+    transition: queryHash.transition || 'default',
     dependencies: [
         { src: 'reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
         { src: 'reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
         { src: 'reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+
+        // Browser Console Speaker Notes
+        { src: 'js/console-notes.js', condition: function() { return !!document.body.classList; } },
+
+		{ src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
         { src: 'reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
-        { src: 'reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
-		
-		// Browser Console Speaker Notes
-        { src: 'js/console-notes.js', async: true, condition: function() { return !!document.body.classList; } }
+        { src: 'reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
     ]
 });
