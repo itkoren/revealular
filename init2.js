843df58b4e88f3755f47838d7941fc48ef4a9b66
itkoren <itkoren@gmail.com>
2014-04-23 16:33:23 +0300

    Update init2.js

--- init2.js
+++ init2.js
@@ -16,7 +16,7 @@ Reveal.initialize({
         // Browser Console Speaker Notes
         { src: 'js/console-notes.js', condition: function() { return !!document.body.classList; } },
 
-		{ src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+	{ src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
         { src: 'reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
         { src: 'reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
     ]
