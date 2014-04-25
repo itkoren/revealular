c489b426c9caa3fa7df19ed9d5c4acaf78324532
itkoren <itkoren@gmail.com>
2014-04-25 11:54:28 +0000

    Add example for remote init file load

---
+++ init-remote.js
@@ -0,0 +1,34 @@
+// Full list of configuration options available here:
+// https://github.com/hakimel/reveal.js#configuration
+var queryHash = Reveal.getQueryHash();
+var scripts;
+var script = document.currentScript;
+if (!script) {
+    scripts = document.getElementsByTagName("script");
+    script = script[scripts.length - 1];
+}
+var path = "";
+var idx = script.src.lastIndexOf("/");
+if (-1 !== idx) {
+    path = script.src.substring(0, idx);
+}
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+    theme: queryHash.theme,
+    transition: queryHash.transition || 'default',
+    dependencies: [
+        { src: path + 'reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+        { src: path + 'reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: path + 'reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+
+        // Browser Console Speaker Notes
+        { src: path + 'js/console-notes.js', condition: function() { return !!document.body.classList; } },
+
+	    { src: path + 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+        { src: path + 'reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+        { src: path + 'reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+    ]
+});
